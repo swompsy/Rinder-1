96556636651da3738a154c68093bb8036cd51489
mewzael <immanuel.santoso@binus.ac.id>
2023-05-25 22:47:40 +0700

    modifying alarmpage and home page for some additonal features

--- app/src/main/java/rinder/com/AlarmActivity.java
+++ app/src/main/java/rinder/com/AlarmActivity.java
@@ -1,14 +1,30 @@
 package rinder.com;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AlarmActivity extends AppCompatActivity {
+
+    private TextView textViewTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        textViewTime = findViewById(R.id.textViewTime);
+
+        // Get the current time
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a", Locale.getDefault());
+        String currentTime = sdf.format(calendar.getTime());
+
+        // Update the TextView with the current time
+        textViewTime.setText(currentTime);
     }
 }
--- app/src/main/java/rinder/com/HomePageActivity.java
+++ app/src/main/java/rinder/com/HomePageActivity.java
@@ -2,13 +2,25 @@ package rinder.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    Button alarmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        alarmButton = findViewById(R.id.AlarmButton);
+
+        alarmButton.setOnClickListener(a -> {
+            Intent intent = new Intent(HomePageActivity.this, AlarmActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
