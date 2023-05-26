d35c1eb35adf2d82650c9f7599c37b7d6b90a796
mewzael <immanuel.santoso@binus.ac.id>
2023-05-27 01:24:50 +0700

    fixing some feature in alarmactivity

--- app/src/main/java/rinder/com/AlarmActivity.java
+++ app/src/main/java/rinder/com/AlarmActivity.java
@@ -8,11 +8,14 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import android.os.CountDownTimer;
 
 
 public class AlarmActivity extends AppCompatActivity {
     private TextView alarmTimeTextView;
     private TextView arrivalTimeTextView;
+
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +44,28 @@ public class AlarmActivity extends AppCompatActivity {
         // Set the arrival time to the TextView
         String arrivalText = "You will be arriving in " + minutes + " minutes ";
         arrivalTimeTextView.setText(arrivalText);
+
+        // Calculate the duration in milliseconds
+        long durationInMillis = minutes * 60 * 1000;
+
+        countDownTimer = new CountDownTimer(durationInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Calculate the remaining time in minutes and seconds
+                int minutes = (int) (millisUntilFinished / (1000 * 60));
+                int seconds = (int) ((millisUntilFinished / 1000) % 60);
+
+                // Update the arrival time
+                String arrivalText = "You will be arriving in " + minutes + " minutes";
+                arrivalTimeTextView.setText(arrivalText);
+            }
+            @Override
+            public void onFinish() {
+                // Perform any actions when the countdown finishes
+                alarmTimeTextView.setText("00:00");
+                // Add your desired actions here (e.g., play alarm sound, display notification)
             }
         };
+        countDownTimer.start();
+    }
+}
