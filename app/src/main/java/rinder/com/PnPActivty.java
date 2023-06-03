69685dfbc44f2c03544b88cf9ef9bdb14bf348a6
mewzael <immanuel.santoso@binus.ac.id>
2023-06-03 23:59:04 +0700

    Fixing UI by adding clock image and the timing of the minutes

---
+++ app/src/main/java/rinder/com/PnPActivty.java
@@ -0,0 +1,14 @@
+package rinder.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PnPActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pnp_activity);
+    }
+}
