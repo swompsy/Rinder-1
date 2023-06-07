1e2223cca1d84ddce78f8ab410fd394a76b8f729
malik <swompsy>
2023-06-07 12:23:59 +0700

    add user location puck

--- app/src/main/java/rinder/com/HomePageActivity.java
+++ app/src/main/java/rinder/com/HomePageActivity.java
@@ -1,21 +1,59 @@
 package rinder.com;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.MapboxMap;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.Plugin;
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener;
+
 
 public class HomePageActivity extends AppCompatActivity {
-
+    private MapView mapView;
     Button alarmButton;
+    private OnIndicatorBearingChangedListener onIndicatorBearingChangedListener = new OnIndicatorBearingChangedListener() {
+        @Override
+        public void onIndicatorBearingChanged(double v) {
+            mapView.getMapboxMap().setCamera(
+                    new CameraOptions.Builder().bearing(v).build()
+            );
+        }
+    };
+    private OnIndicatorPositionChangedListener onIndicatorPositionChangedListener = new OnIndicatorPositionChangedListener() {
+        @Override
+        public void onIndicatorPositionChanged(@NonNull Point point) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().center(point).build());
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        mapView = findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(
+                Style.MAPBOX_STREETS,
+                new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        initLocationComponent();
+                    }
+                }
 
+        );
         alarmButton = findViewById(R.id.AlarmButton);
 
         alarmButton.setOnClickListener(a -> {
@@ -23,5 +61,51 @@ public class HomePageActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+
+
+    }
+
+    private void initLocationComponent() {
+        Integer id = Integer.valueOf(R.id.mapView);
+        LocationComponentPlugin locationComponentPlugin = mapView.getPlugin(Plugin.MAPBOX_LOCATION_COMPONENT_PLUGIN_ID);
+        locationComponentPlugin.updateSettings(settings -> {
+            settings.setEnabled(true);
+            settings.setPulsingEnabled(true);
+            return null;
+        });
+        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+        locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mapView != null) {
+            mapView.onStart();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mapView != null) {
+            mapView.onStop();
+        }
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mapView != null) {
+            mapView.onLowMemory();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mapView != null) {
+            mapView.onDestroy();
+        }
     }
 }
